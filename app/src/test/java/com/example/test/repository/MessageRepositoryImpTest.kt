6e96fe9c3faf11abfaf4a5bbd0f9be239952f8bf
Juan Manuel Fernandez <jmfer22@gmail.com>
2023-12-14 14:30:07 -0700

    Adding Unit Tests

--- app/src/test/java/com/example/test/repository/MessageRepositoryImpTest.kt
+++ app/src/test/java/com/example/test/repository/MessageRepositoryImpTest.kt
@@ -1,14 +1,15 @@
 import com.example.test.repository.MessageRepositoryImp
 import com.example.test.repository.model.ApiResponse
+import com.example.test.repository.model.toSendMessageRequest
 import com.example.test.repository.remote.ApiService
 import com.example.test.ui.model.MessageToSend
 import com.example.test.ui.model.UIMessage
 import io.mockk.coEvery
-import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import okhttp3.ResponseBody
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -28,126 +29,126 @@ class MessageRepositoryImpTest {
     }
 
     @Test
-    fun `getMessages returns empty list on unsuccessful response`() = runBlocking {
-        // Arrange
-        val responseBody = mockk<ResponseBody> {
-            every { contentType() } returns okhttp3.MediaType.get("application/json")
-        }
-        val response = mockk<Response<ApiResponse>> {
-            every { isSuccessful } returns true
-            every { body() } returns responseBody
-        }
-        coEvery { apiService.getMessages() } returns Response.error(400, response.errorBody()!!)
+    fun `test get messages with empty list`() = runBlocking {
+        val successfulResponse = Response.success(ApiResponse(200, "{}"))
+        coEvery { apiService.getMessages() } returns successfulResponse
 
-        // Act
         val result = messageRepository.getMessages()
 
-        // Assert
-        assertEquals(emptyList<UIMessage>(), result)
+        assertEquals(listOf<UIMessage>(), result)
     }
 
     @Test
-    fun `getMessages returns empty list on successful response with non-200 status code`() =
-        runBlocking {
-            // Arrange
-            val responseBody = mockk<ApiResponse>()
-            coEvery { apiService.getMessages() } returns Response.success(404, responseBody)
-
-            // Act
-            val result = messageRepository.getMessages()
-
-            // Assert
-            assertEquals(emptyList<UIMessage>(), result)
-        }
-
-    @Test
-    fun `getMessages returns list of UIMessage on successful response with 200 status code`() =
-        runBlocking {
-            // Arrange
-            val responseBody = mockk<ApiResponse>()
-            coEvery { responseBody.statusCode } returns 200
-            coEvery { apiService.getMessages() } returns Response.success(responseBody)
-
-            // Act
-            val result = messageRepository.getMessages()
-
-            // Assert
-            // Add assertions based on your implementation, e.g., check if the conversion is correct
-            // verify(responseBody).toUIMessage()
-        }
-
-    @Test
-    fun `sendMessage returns true on successful response`() = runBlocking {
-        // Arrange
-        val messageToSend = mockk<MessageToSend>()
-        val apiResponse = mockk<ApiResponse>()
-        coEvery { apiService.sendMessage(any()) } returns Response.success(200, apiResponse)
-
-        // Act
-        val result = messageRepository.sendMessage(messageToSend)
-
-        // Assert
-        assertEquals(true, result)
-    }
-
-    @Test
-    fun `sendMessage returns false on unsuccessful response`() = runBlocking {
-        // Arrange
-        val messageToSend = mockk<MessageToSend>()
-        coEvery { apiService.sendMessage(any()) } returns Response.error(500, mockk())
-
-        // Act
-        val result = messageRepository.sendMessage(messageToSend)
-
-        // Assert
-        assertEquals(false, result)
-    }
-
-    @Test
-    fun `searchByUser returns empty list on unsuccessful response`() = runBlocking {
-        // Arrange
-        val user = "testUser"
-        coEvery { apiService.getMessagesByUser(user) } returns Response.error(401, mockk())
-
-        // Act
-        val result = messageRepository.searchByUser(user)
-
-        // Assert
-        assertEquals(emptyList<UIMessage>(), result)
-    }
-
-    @Test
-    fun `searchByUser returns empty list on successful response with non-200 status code`() =
-        runBlocking {
-            // Arrange
-            val user = "testUser"
-            val responseBody = mockk<ApiResponse>()
-            coEvery { apiService.getMessagesByUser(user) } returns Response.success(
-                403,
-                responseBody
+    fun `test get messages with full list`() = runBlocking {
+        val successfulResponse = Response.success(
+            ApiResponse(
+                200,
+                "{\"juan\": [{\"subject\": \"2\", \"message\": \"Hello World\"}]}"
             )
+        )
+        coEvery { apiService.getMessages() } returns successfulResponse
 
-            // Act
-            val result = messageRepository.searchByUser(user)
+        val result = messageRepository.getMessages()
 
-            // Assert
-            assertEquals(emptyList<UIMessage>(), result)
-        }
+        assertEquals(listOf(UIMessage("juan", "2", "Hello World")), result)
+    }
 
     @Test
-    fun `searchByUser returns list of UIMessage on successful response with 200 status code`() =
-        runBlocking {
-            // Arrange
-            val user = "testUser"
-            val responseBody = mockk<ApiResponse>()
-            coEvery { responseBody.statusCode } returns 200
-            coEvery { apiService.getMessagesByUser(user) } returns Response.success(responseBody)
+    fun `test get messages with status code dif 200`() = runBlocking {
+        val successfulResponse = Response.success(ApiResponse(400, "{}"))
+        coEvery { apiService.getMessages() } returns successfulResponse
 
-            // Act
-            val result = messageRepository.searchByUser(user)
+        val result = messageRepository.getMessages()
 
-            // Assert
-            // Add assertions based on your implementation, e.g., check if the conversion is correct
-            // verify(responseBody).toUIMessageByUser()
-        }
+        assertEquals(listOf<UIMessage>(), result)
+    }
+
+    @Test
+    fun `test get messages with failure`() = runBlocking {
+        // Mock a failure response
+        val errorResponse = Response.error<ApiResponse>(404, ResponseBody.create(null, "Not Found"))
+        coEvery { apiService.getMessages() } returns errorResponse
+
+        val result = messageRepository.getMessages()
+
+        // Verify that the result is an empty list in case of failure
+        assertEquals(emptyList<UIMessage>(), result)
+    }
+
+    @Test
+    fun `test send messages`() = runBlocking {
+        val successfulResponse = Response.success(ApiResponse(200, "{}"))
+        val messageRequest = MessageToSend("Juan", "Test", "Hello World")
+        coEvery { apiService.sendMessage(messageRequest.toSendMessageRequest()) } returns successfulResponse
+
+        val result = messageRepository.sendMessage(messageRequest)
+
+        assert(result)
+    }
+
+    @Test
+    fun `test send message with failure`() = runBlocking {
+        // Mock a failure response
+        val errorResponse = Response.error<ApiResponse>(404, ResponseBody.create(null, "Not Found"))
+        val messageRequest = MessageToSend("Juan", "Test", "Hello World")
+        coEvery { apiService.sendMessage(messageRequest.toSendMessageRequest()) } returns errorResponse
+
+        val result = messageRepository.sendMessage(messageRequest)
+
+        // Verify that the result is an empty list in case of failure
+        assertFalse(result)
+    }
+
+    @Test
+    fun `test get messages by user with empty list`() = runBlocking {
+        val successfulResponse = Response.success(
+            ApiResponse(
+                404, "{\n" +
+                        "    \"body\": \"\\\"resource not found\\\"\"\n" +
+                        "}"
+            )
+        )
+        coEvery { apiService.getMessagesByUser("Juan") } returns successfulResponse
+
+        val result = messageRepository.searchByUser("Juan")
+
+        assertEquals(listOf<UIMessage>(), result)
+    }
+
+    @Test
+    fun `test get messages by user with full list`() = runBlocking {
+        val successfulResponse = Response.success(
+            ApiResponse(
+                200,
+                "{\"user\": \"Juan\", \"message\": [{\"subject\": \"2\", \"message\": \"Hello World\"}]}"
+            )
+        )
+        coEvery { apiService.getMessagesByUser("Juan") } returns successfulResponse
+
+        val result = messageRepository.searchByUser("Juan")
+
+        assertEquals(listOf(UIMessage("Juan", "2", "Hello World")), result)
+    }
+
+    @Test
+    fun `test get messages by user with status code dif 200`() = runBlocking {
+        val successfulResponse = Response.success(ApiResponse(400, "{}"))
+        coEvery { apiService.getMessagesByUser("Juan") } returns successfulResponse
+
+        val result = messageRepository.searchByUser("Juan")
+
+        assertEquals(listOf<UIMessage>(), result)
+    }
+
+    @Test
+    fun `test get messages by user with failure`() = runBlocking {
+        // Mock a failure response
+        val errorResponse = Response.error<ApiResponse>(404, ResponseBody.create(null, "Not Found"))
+        coEvery { apiService.getMessagesByUser("Juan") } returns errorResponse
+
+        val result = messageRepository.searchByUser("Juan")
+
+        // Verify that the result is an empty list in case of failure
+        assertEquals(emptyList<UIMessage>(), result)
+    }
 }
